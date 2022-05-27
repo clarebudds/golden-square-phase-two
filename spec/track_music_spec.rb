212962c439acf1ff85ed272d07219a45f3b71a72
Clare Budds <clarebudds@yahoo.co.uk>
2022-05-27 18:34:37 +0100

    Phase2 Step6 challenge

--- spec/track_music_spec.rb
+++ spec/track_music_spec.rb
@@ -2,7 +2,7 @@ require "track_music"
 
 RSpec.describe TrackMusic do
   context "given no music track" do
-    it "has an empty list" do
+    it "return nothing, has an empty list" do
       track_music = TrackMusic.new
       expect(track_music.list).to eq []
     end
@@ -16,8 +16,8 @@ RSpec.describe TrackMusic do
     end
   end
 
-  context "given two tracks" do
-    it "includes each music track entered in the list" do
+  context "given multiple tracks" do
+    it "includes each music track in the list" do
       track_music = TrackMusic.new
       track_music.add("Spaceman") 
       track_music.add("Superstition") 
@@ -26,48 +26,6 @@ RSpec.describe TrackMusic do
   end
 end
 
-# require "todo_list"
 
-# # RSpec.describe TodoList do
-# #   context "given no tasks" do
-#     it "has an empty list" do
-#       todo_list = TodoList.new
-#       expect(todo_list.list).to eq []
-#     end
-#   end
 
-#   context "given a task" do
-#     it "includes the task in the list" do
-#       todo_list = TodoList.new
-#       todo_list.add("Hoover the stairs") 
-#       expect(todo_list.list).to eq ["Hoover the stairs"]
-#     end
-#   end
 
-#   context "given two tasks" do
-#     it "includes both tasks in the list" do
-#       todo_list = TodoList.new
-#       todo_list.add("Hoover the stairs") 
-#       todo_list.add("Clean the car") 
-#       expect(todo_list.list).to eq ["Hoover the stairs", "Clean the car"]
-#     end
-#   end
-
-#   context "when we complete a task" do
-#     it "removes the task from the list" do
-#       todo_list = TodoList.new
-#       todo_list.add("Hoover the stairs") 
-#       todo_list.add("Clean the car") 
-#       todo_list.complete("Clean the car") 
-#       expect(todo_list.list).to eq ["Hoover the stairs"]
-#     end
-#   end
-
-#   context "when we try to complete a non-existent task" do
-#     it "fails" do
-#       todo_list = TodoList.new
-#       todo_list.add("Hoover the stairs") 
-#       expect{ todo_list.complete("Hoover the cat") }.to raise_error "No such task."
-#     end
-#   end
-# end
